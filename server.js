2a88f4006155e6125aa9542c5aa2e3da774f8f5b
Matheus Honorato de Sousa <102932606+MatheusHonoratoDev@users.noreply.github.com>
2024-01-31 17:16:19 -0300

    correções

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/crud-angular')));
+app.use(express.json());
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/crud-angular/index.html'));
