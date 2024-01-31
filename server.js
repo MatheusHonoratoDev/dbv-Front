88a480f800e5220d21da47e901021c50d7cfadb4
Matheus Honorato de Sousa <102932606+MatheusHonoratoDev@users.noreply.github.com>
2024-01-31 16:11:35 -0300

    agora vai

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/crud-angular'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/crud-angular/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor está rodando na porta ${PORT}`);
+});
