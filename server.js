305de0f46c8d6ba8ec609af3dcf54991838bf5d9
Matheus Honorato de Sousa <102932606+MatheusHonoratoDev@users.noreply.github.com>
2024-01-31 17:31:55 -0300

    aaaa

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/crud-angular')));
-app.use(express.json());
+const distPath = path.join(__dirname, 'dist', 'crud-angular');
+
+app.use(express.static(distPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/crud-angular/index.html'));
+  res.sendFile(path.join(distPath, 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
